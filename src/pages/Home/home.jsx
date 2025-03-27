b8620b8e661216026f26b05ca4c12dd1e93aafc7
BlesonPhilip <blesonphilip21@gmail.com>
2025-03-27 21:07:11 +0530

    home age and get the data from the file is work

--- src/pages/Home/home.jsx
+++ src/pages/Home/home.jsx
@@ -1,16 +1,36 @@
 import React from "react";
 import "./home.css";
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 const Home = () => {
-  const [products, setProducts] = useState();
+  const [products, setProducts] = useState([]);
+
+  const getProducts = async () => {
+    const response = await axios.get("http://localhost:3000/detail");
+    setProducts(response.data);
+    // console.log(response);
+  };
+  useEffect(() => {
+    getProducts(), [];
+  });
   return (
     <>
       <div className="head">
         <h3>Your Products</h3>
         <hr />
       </div>
-      <div className="product-contanier">{Map.product}</div>
+      <div className="product-container">
+        {products.map((item) => {
+          return (
+            <div className="product-card">
+              <h3>{item.title}</h3>
+              <p>{item.price}</p>
+              <p>{item.description}</p>
+            </div>
+          );
+        })}
+      </div>
     </>
   );
 };
